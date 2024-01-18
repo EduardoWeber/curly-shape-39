c9f60438e3730f0dc94029a1a470394760004700
django <django@bee4c1f217b0>
2024-01-18 13:16:03 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "curly-shape-39",
+  url: "https://curly-shape-39.botics.co",
+  api: "https://curly-shape-39.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
